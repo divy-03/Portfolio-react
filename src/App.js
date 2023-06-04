97fd72a5e4e39a9d1dcbf00e89b997b40eb34b2d
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-06-04 22:48:19 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("Dark");
+
+  const toggleMode = () => {
+    if (mode === "Dark") {
+      setMode("Light");
+    } 
+    else {
+      setMode("Dark");
+    }
+  };
+
   return (
     <>
-      <Navbar/>
+      <Navbar mode={mode} toggleMode={toggleMode}/>
     </>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
-function Navbar() {
+function Navbar(props) {
+
+    const [isVisible, setIsVisible] = useState("v-hidden")
+
+    const toggleMenu = () => {
+        setIsVisible(isVisible === "v-hidden" ? "v-visible" : "v-hidden");
+    }
+
+
   return (
     <>
-      <nav className="navbar flex-row">
+      <nav className={`navbar${props.mode} flex-row`}>
         <div className="nav-left flex-row">
             <div className="icon">
                 Port
@@ -13,9 +22,7 @@ function Navbar() {
                 folio
             </div>
         </div>
-        <div className="nav-cent">
-        
-        </div>
+
         <div className="nav-right flex-row">
             <div className="nav-menu">
                 <ul className="nav-list flex-row">
@@ -29,9 +36,9 @@ function Navbar() {
                 <a href="/" className="CV-btn" download>Download CV</a>
             </div>
         </div>
+        <button className='btn-menu' onClick={toggleMenu}>#</button>
         {/* <!-- This is the menuToggle list --> */}
-        <div className="menuToggle">
-            <div className="nav-resp">
+            <div className={`nav-resp${props.mode} ${isVisible}`}>
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="#about">About Me</a></li>
@@ -39,8 +46,8 @@ function Navbar() {
                     <li><a href="#contact">Contact Us</a></li>
                     <li><a href="/" className="CV-btn-resp" download>DownloadCV</a></li>
                 </ul>
+                <button onClick={props.toggleMode} className={`btn-${props.mode}`}>{props.mode} Mode</button>
             </div>
-        </div>
     </nav>
     </>
   )
