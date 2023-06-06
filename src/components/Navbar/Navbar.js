256a558131c351250227ed652c52a6af49b659de
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-06-06 11:56:17 +0530

    navBar completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css'
-import { isVisible } from '@testing-library/user-event/dist/utils';
+// import { isVisible } from '@testing-library/user-event/dist/utils';
 
 function Navbar(props) {
 
@@ -25,15 +25,16 @@ function Navbar(props) {
 
         <div className="nav-right flex-row">
             <div className="nav-menu">
-                <ul className="nav-list flex-row">
+                <ul className={`nav-list${props.mode} flex-row`}>
                     <li><a href="/">Home</a></li>
                     <li><a href="#about">About Me</a></li>
                     <li><a href="/">Services</a></li>
                     <li><a href="#contact">Contact Us</a></li>
                 </ul>
             </div>
-            <div className="download">
+            <div className="download flex-row">
                 <a href="/" className="CV-btn" download>Download CV</a>
+                <button onClick={props.toggleMode} className={`btn-${props.mode}`}>{props.mode} Mode</button>
             </div>
         </div>
         <button className='btn-menu' onClick={toggleMenu}>#</button>
@@ -45,8 +46,8 @@ function Navbar(props) {
                     <li><a href="/">Services</a></li>
                     <li><a href="#contact">Contact Us</a></li>
                     <li><a href="/" className="CV-btn-resp" download>DownloadCV</a></li>
-                </ul>
                 <button onClick={props.toggleMode} className={`btn-${props.mode}`}>{props.mode} Mode</button>
+                </ul>
             </div>
     </nav>
     </>
